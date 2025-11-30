84c8d35c9ba052d27e34abdf1346755485cee675
Shadenm-404 <yourEmail@example.com>
2025-11-30 07:34:19 +0000

    Add task 2 - args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    count = len(args)
+
+    if count == 0:
+        print("0 arguments.")
+    else:
+        word = "argument" if count == 1 else "arguments"
+        print("{} {}:".format(count, word))
+
+        for i in range(count):
+            print("{}: {}".format(i + 1, args[i]))
