7ee0c5b43a871e6652fd21453141704c3200a495
Shadenm-404 <viuxwl7@gmail.com>
2025-12-20 06:43:04 +0000

    Task 1: MyList (checker compliant)

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-Module that defines MyList class which extends the built-in list class.
-Allows printing the list in sorted (ascending) order without modifying it.
-"""
+"""Defines a class MyList that inherits from list."""
 
 
 class MyList(list):
-    """
-    A class that inherits from list and adds a print_sorted method.
-    """
+    """MyList class."""
 
     def print_sorted(self):
-        """
-        Prints the list sorted in ascending order without modifying the.
-        """
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
