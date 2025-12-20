bc3d5a4beeea84a9bb6dc0c293a4f894cf525bf3
Shadenm-404 <rahaf@rahfr.sa>
2025-12-20 12:40:24 +0000

    Fix ValueError message output for integer_validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Validates an integer"""
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError
+            raise ValueError(name + " must be greater than 0"
