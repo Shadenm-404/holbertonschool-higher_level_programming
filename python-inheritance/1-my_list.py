9f2d34a2e59885fde8ac4b46b2d6bf522525199c
Shadenm-404 <viuxwl7@gmail.com>
2025-12-20 06:12:20 +0000

    Task 1: MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class MyList that inherits from list."""
+"""
+Module that defines MyList class which extends the built-in list class.
+Allows printing the list in sorted (ascending) order without modifying it.
+"""
 
 
 class MyList(list):
-    """MyList class."""
+    """
+    A class that inherits from list and adds a print_sorted method.
+    """
 
     def print_sorted(self):
-        """Prints the list sorted in ascending order."""
+        """
+        Prints the list sorted in ascending order without modifying the.
+        """
         print(sorted(self))
-
