f0a1cedae7e1c4054295f2aa692dfac3b70c0d8e
Shadenm-404 <viuxwl7@gmail.com>
2025-12-20 06:09:53 +0000

    Fix task 1 MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines a class MyList that inherits from list.
-"""
+"""Defines a class MyList that inherits from list."""
 
 
 class MyList(list):
-    """
-    MyList is a subclass of list that provides
-    a method to print the list sorted in ascending order.
-    """
+    """MyList class."""
 
     def print_sorted(self):
-        """
-        Prints the list sorted in ascending order.
-        """
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
+
