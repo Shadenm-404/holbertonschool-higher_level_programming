3549a51e19286f7158af724655b5f9f34e1639b9
Shadenm-404 <viuxwl7@gmail.com>
2025-12-26 06:20:40 +0000

    Fix PEP8: split long docstring line

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Module that contains a function to write a string to a file."""
+"""Module that contains a function to write to a file."""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns number of characters."""
+    """
+    Writes a string to a text file (UTF8)
+    and returns the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
