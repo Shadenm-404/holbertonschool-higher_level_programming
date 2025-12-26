120944d6aec4d9fd073570dc9c079e3d4497769f
Shadenm-404 <viuxwl7@gmail.com>
2025-12-26 06:35:52 +0000

    Add task 11: Student serialization and reload support

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with serialization support."""
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+        If attrs is a list, only selected attributes are included.
+        """
+        if isinstance(attrs, list):
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        using a dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
