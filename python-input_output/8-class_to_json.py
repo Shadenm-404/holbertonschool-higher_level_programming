49a09abe122e701799efdb374c52b625b05b67fb
Shadenm-404 <viuxwl7@gmail.com>
2025-12-26 06:38:32 +0000

    Fix PEP8: shorten docstring in class_to_json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Module that converts a class instance to a dictionary for JSON."""
+"""Module that converts a class instance to a dictionary."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization."""
+    """
+    Returns the dictionary description of an object
+    for JSON serialization.
+    """
     return obj.__dict__
