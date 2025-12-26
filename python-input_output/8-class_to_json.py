660e2062d90557821bb5f7595a81351f9aff5f9e
Shadenm-404 <viuxwl7@gmail.com>
2025-12-26 06:30:35 +0000

    Add task 8: class_to_json function

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that converts a class instance to a dictionary for JSON."""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description of an object for JSON serialization."""
+    return obj.__dict__
