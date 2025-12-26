6f9e54bf5cdc03efbb00e677812ef39a76b1cab8
Shadenm-404 <viuxwl7@gmail.com>
2025-12-26 06:33:37 +0000

    Add task 10: Student class with filtered JSON output

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON filter support."""
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+        If attrs is a list of strings, only attributes in the list are included.
+        """
+        if isinstance(attrs, list):
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
+        return self.__dict__
