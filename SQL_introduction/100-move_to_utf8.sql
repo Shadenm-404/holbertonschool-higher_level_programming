7c2db3fe36afc9e13282d1237d6ac114970a2d69
Shadenm-404 <viuxwl7@gmail.com>
2026-01-11 09:06:13 +0000

    Fix UTF8 conversion by selecting database

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -4,6 +4,9 @@ ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+-- Select the database
+USE hbtn_0c_0;
+
 -- Converts the table first_table to UTF8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
