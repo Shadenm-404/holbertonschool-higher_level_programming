92981c659fcac3d49aad926e5666625192e04b91
Shadenm-404 <viuxwl7@gmail.com>
2026-01-11 09:04:20 +0000

    Convert database and tables to utf8mb4

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Converts the database hbtn_0c_0 to UTF8
+-- Database conversion
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Converts the table first_table to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Converts the name field to UTF8
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+COLLATE utf8mb4_unicode_ci;
